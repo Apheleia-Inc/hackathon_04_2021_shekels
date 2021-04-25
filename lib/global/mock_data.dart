604ea7324ed19506942892ebb171dd1b3f8c9609
WillowWisp <byakuran614@gmail.com>
2021-04-25 18:55:48 +0700

    Add internet permission. Update mock data.

--- lib/global/mock_data.dart
+++ lib/global/mock_data.dart
@@ -4,212 +4,16 @@ import 'package:uuid/uuid.dart';
 
 class MockData {
   static List<PromotionDto> getVisaPromos(LinkedCardDto card) {
-    return [
-      PromotionDto(
-        id: Uuid().v4(),
-        name: "Handcrafted Fresh Salad",
-        category: "Generic",
-        description: "\$9 Cashback",
-        store: StoreDto(
-          name: "Starbucks",
-          imageUrl:
-              "https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png",
-          url: "https://www.starbucks.com/",
-        ),
-        isApplied: false,
-        expirationDate: DateTime.now(),
-        fromCard: card,
-      ),
-      PromotionDto(
-        id: Uuid().v4(),
-        name: "Gorgeous Granite Computer",
-        category: "Tasty",
-        description: "\$1 Cashback",
-        store: StoreDto(
-          name: "Turbo Tax",
-          imageUrl:
-              "https://blog.turbotax.intuit.com/wp-content/uploads/2019/04/cropped-favicon.png",
-          url: "https://turbotax.intuit.com/",
-        ),
-        isApplied: false,
-        expirationDate: DateTime.now(),
-        fromCard: card,
-      ),
-      PromotionDto(
-        id: Uuid().v4(),
-        name: "Rustic Granite Bacon",
-        category: "Sleek",
-        description: "67% Off",
-        store: StoreDto(
-          name: "Amazon",
-          imageUrl:
-              "http://media.corporate-ir.net/media_files/IROL/17/176060/Oct18/Amazon%20logo.PNG",
-          url: "https://www.amazon.com/",
-        ),
-        isApplied: false,
-        expirationDate: DateTime.now(),
-        fromCard: card,
-      ),
-      PromotionDto(
-        id: Uuid().v4(),
-        name: "Refined Rubber Shoes",
-        category: "Gorgeous",
-        description: "49% Off",
-        store: StoreDto(
-          name: "Dell",
-          imageUrl:
-              "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Dell_Logo.svg/480px-Dell_Logo.svg.png",
-          url: "http://dell.com/",
-        ),
-        isApplied: false,
-        expirationDate: DateTime.now(),
-        fromCard: card,
-      ),
-      PromotionDto(
-        id: Uuid().v4(),
-        name: "Intelligent Concrete Mouse",
-        category: "Practical",
-        description: "\$40 Off",
-        store: StoreDto(
-          name: "Turbo Tax",
-          imageUrl:
-              "https://blog.turbotax.intuit.com/wp-content/uploads/2019/04/cropped-favicon.png",
-          url: "https://turbotax.intuit.com/",
-        ),
-        isApplied: false,
-        expirationDate: DateTime.now(),
-        fromCard: card,
-      ),
-      PromotionDto(
-        id: Uuid().v4(),
-        name: "Awesome Soft Shirt",
-        category: "Fantastic",
-        description: "\$61 Off",
-        store: StoreDto(
-          name: "Amazon",
-          imageUrl:
-              "http://media.corporate-ir.net/media_files/IROL/17/176060/Oct18/Amazon%20logo.PNG",
-          url: "https://www.amazon.com/",
-        ),
-        isApplied: false,
-        expirationDate: DateTime.now(),
-        fromCard: card,
-      ),
-      PromotionDto(
-        id: Uuid().v4(),
-        name: "Licensed Rubber Computer",
-        category: "Small",
-        description: "\$78 Off",
-        store: StoreDto(
-          name: "Dell",
-          imageUrl:
-              "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Dell_Logo.svg/480px-Dell_Logo.svg.png",
-          url: "http://dell.com/",
-        ),
-        isApplied: false,
-        expirationDate: DateTime.now(),
-        fromCard: card,
-      ),
-      PromotionDto(
-        id: Uuid().v4(),
-        name: "Handmade Concrete Shoes",
-        category: "Incredible",
-        description: "\$9 Cashback",
-        store: StoreDto(
-          name: "Amazon",
-          imageUrl:
-              "http://media.corporate-ir.net/media_files/IROL/17/176060/Oct18/Amazon%20logo.PNG",
-          url: "https://www.amazon.com/",
-        ),
-        isApplied: false,
-        expirationDate: DateTime.now(),
-        fromCard: card,
-      ),
-      PromotionDto(
-        id: Uuid().v4(),
-        name: "Fantastic Granite Tuna",
-        category: "Awesome",
-        description: "\$8 Cashback",
-        store: StoreDto(
-          name: "Google Play",
-          imageUrl:
-              "https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/103-GooglePlay_play_google_play_apps-512.png",
-          url: "https://play.google.com/",
-        ),
-        isApplied: false,
-        expirationDate: DateTime.now(),
-        fromCard: card,
-      ),
-    ];
+    return [];
   }
 
   static List<PromotionDto> getMasterCardPromos(LinkedCardDto card) {
     return [
       PromotionDto(
         id: Uuid().v4(),
-        name: "Handmade Fresh Tuna",
-        category: "Handcrafted",
-        description: "\$36 Off",
-        store: StoreDto(
-          name: "Turbo Tax",
-          imageUrl:
-              "https://blog.turbotax.intuit.com/wp-content/uploads/2019/04/cropped-favicon.png",
-          url: "https://turbotax.intuit.com/",
-        ),
-        isApplied: false,
-        expirationDate: DateTime.now(),
-        fromCard: card,
-      ),
-      PromotionDto(
-        id: Uuid().v4(),
-        name: "Tasty Frozen Shoes",
-        category: "Refined",
-        description: "87% Off",
-        store: StoreDto(
-          name: "Starbucks",
-          imageUrl:
-              "https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png",
-          url: "https://www.starbucks.com/",
-        ),
-        isApplied: false,
-        expirationDate: DateTime.now(),
-        fromCard: card,
-      ),
-      PromotionDto(
-        id: Uuid().v4(),
-        name: "Practical Granite Bacon",
-        category: "Incredible",
-        description: "\$7 Cashback",
-        store: StoreDto(
-          name: "Turbo Tax",
-          imageUrl:
-              "https://blog.turbotax.intuit.com/wp-content/uploads/2019/04/cropped-favicon.png",
-          url: "https://turbotax.intuit.com/",
-        ),
-        isApplied: false,
-        expirationDate: DateTime.now(),
-        fromCard: card,
-      ),
-      PromotionDto(
-        id: Uuid().v4(),
-        name: "Refined Metal Tuna",
-        category: "Sleek",
-        description: "1% Cashback",
-        store: StoreDto(
-          name: "Starbucks",
-          imageUrl:
-              "https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png",
-          url: "https://www.starbucks.com/",
-        ),
-        isApplied: false,
-        expirationDate: DateTime.now(),
-        fromCard: card,
-      ),
-      PromotionDto(
-        id: Uuid().v4(),
-        name: "Refined Soft Tuna",
-        category: "Handmade",
-        description: "80% Cashback",
+        name: "Next Movie",
+        category: "Generic",
+        description: "2\% Cashback",
         store: StoreDto(
           name: "Google Play",
           imageUrl:
@@ -222,9 +26,155 @@ class MockData {
       ),
       PromotionDto(
         id: Uuid().v4(),
-        name: "Unbranded Wooden Keyboard",
+        name: "Fillup",
+        category: "Tasty",
+        description: "\$18 Off",
+        store: StoreDto(
+          name: "rompetrol",
+          imageUrl:
+              "https://seeklogo.com/images/R/rompetrol-logo-5CA660BD8A-seeklogo.com.png",
+          url: "https://www.rompetrol.md/",
+        ),
+        isApplied: false,
+        expirationDate: DateTime.now(),
+        fromCard: card,
+      ),
+      PromotionDto(
+        id: Uuid().v4(),
+        name: "Elite Tier",
+        category: "Fintech",
+        description: "10% Off",
+        store: StoreDto(
+          name: "Stockwise",
+          imageUrl: "https://app.stockwise.io/static/media/Icon.026536ed.png",
+          url: "https://stockwise.io/",
+        ),
+        isApplied: false,
+        expirationDate: DateTime.now(),
+        fromCard: card,
+      ),
+      PromotionDto(
+        id: Uuid().v4(),
+        name: "All In Store",
+        category: "Gorgeous",
+        description: "15% Off",
+        store: StoreDto(
+          name: "SPORTLANDIA",
+          imageUrl: "https://sportlandia.md/assets/img/logo.png",
+          url: "https://sportlandia.md/",
+        ),
+        isApplied: false,
+        expirationDate: DateTime.now(),
+        fromCard: card,
+      ),
+      PromotionDto(
+        id: Uuid().v4(),
+        name: "25 Liters",
+        category: "Practical",
+        description: "\$30 Cashback",
+        store: StoreDto(
+          name: "rompetrol",
+          imageUrl:
+              "https://seeklogo.com/images/R/rompetrol-logo-5CA660BD8A-seeklogo.com.png",
+          url: "https://www.rompetrol.md/",
+        ),
+        isApplied: false,
+        expirationDate: DateTime.now(),
+        fromCard: card,
+      ),
+      PromotionDto(
+        id: Uuid().v4(),
+        name: "Next Purchase",
+        category: "Fantastic",
+        description: "\$29 Off",
+        store: StoreDto(
+          name: "NVIDIA",
+          imageUrl:
+              "https://img.favpng.com/18/20/13/nvidia-logo-geforce-intel-graphics-processing-unit-png-favpng-XSEbZScDrdBvrVF5T5QJkzcr2.jpg",
+          url: "https://www.nvidia.com/",
+        ),
+        isApplied: false,
+        expirationDate: DateTime.now(),
+        fromCard: card,
+      ),
+      PromotionDto(
+        id: Uuid().v4(),
+        name: "Vin Pastoral",
+        category: "Small",
+        description: "\$23 Off",
+        store: StoreDto(
+          name: "Nr1",
+          imageUrl: "https://nr1.md/public/images/logo.png",
+          url: "https://nr1.md/",
+        ),
+        isApplied: false,
+        expirationDate: DateTime.now(),
+        fromCard: card,
+      ),
+      PromotionDto(
+        id: Uuid().v4(),
+        name: "All In Store",
+        category: "Incredible",
+        description: "34\% Cashback",
+        store: StoreDto(
+          name: "SPORTLANDIA",
+          imageUrl: "https://sportlandia.md/assets/img/logo.png",
+          url: "https://sportlandia.md/",
+        ),
+        isApplied: false,
+        expirationDate: DateTime.now(),
+        fromCard: card,
+      ),
+      PromotionDto(
+        id: Uuid().v4(),
+        name: "25 Liters",
+        category: "Awesome",
+        description: "\$44 Off",
+        store: StoreDto(
+          name: "rompetrol",
+          imageUrl:
+              "https://seeklogo.com/images/R/rompetrol-logo-5CA660BD8A-seeklogo.com.png",
+          url: "https://www.rompetrol.md/",
+        ),
+        isApplied: false,
+        expirationDate: DateTime.now(),
+        fromCard: card,
+      ),
+      PromotionDto(
+        id: Uuid().v4(),
+        name: "Next Purchase",
         category: "Handcrafted",
-        description: "\$5 Cashback",
+        description: "\$25 Off",
+        store: StoreDto(
+          name: "NVIDIA",
+          imageUrl:
+              "https://img.favpng.com/18/20/13/nvidia-logo-geforce-intel-graphics-processing-unit-png-favpng-XSEbZScDrdBvrVF5T5QJkzcr2.jpg",
+          url: "https://www.nvidia.com/",
+        ),
+        isApplied: false,
+        expirationDate: DateTime.now(),
+        fromCard: card,
+      ),
+      PromotionDto(
+        id: Uuid().v4(),
+        name: "Home Loan, First House",
+        category: "Refined",
+        description: "\$500 Off per Month",
+        store: StoreDto(
+          name: "MAIB",
+          imageUrl:
+              "https://fci.nl/sites/default/files/MAIB%20Moldova%20Agroindbank.jpg",
+          url: "https://www.maib.md/",
+        ),
+        isApplied: false,
+        expirationDate: DateTime.now(),
+        fromCard: card,
+      ),
+      PromotionDto(
+        id: Uuid().v4(),
+        name: "Next Purchase",
+        category: "Incredible",
+        description: "\$7 Cashback",
         store: StoreDto(
           name: "Amazon",
           imageUrl:
@@ -237,14 +187,13 @@ class MockData {
       ),
       PromotionDto(
         id: Uuid().v4(),
-        name: "Sleek Fresh Cheese",
-        category: "Fantastic",
-        description: "\$2 Cashback",
+        name: "All In Store",
+        category: "Sleek",
+        description: "23% Cashback",
         store: StoreDto(
-          name: "Starbucks",
-          imageUrl:
-              "https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png",
-          url: "https://www.starbucks.com/",
+          name: "SPORTLANDIA",
+          imageUrl: "https://sportlandia.md/assets/img/logo.png",
+          url: "https://sportlandia.md/",
         ),
         isApplied: false,
         expirationDate: DateTime.now(),
@@ -252,14 +201,59 @@ class MockData {
       ),
       PromotionDto(
         id: Uuid().v4(),
-        name: "Incredible Soft Towels",
+        name: "Fillup",
+        category: "Handmade",
+        description: "\$2 Cashback",
+        store: StoreDto(
+          name: "rompetrol",
+          imageUrl:
+              "https://seeklogo.com/images/R/rompetrol-logo-5CA660BD8A-seeklogo.com.png",
+          url: "https://www.rompetrol.md/",
+        ),
+        isApplied: false,
+        expirationDate: DateTime.now(),
+        fromCard: card,
+      ),
+      PromotionDto(
+        id: Uuid().v4(),
+        name: "Credit Cappuccino",
+        category: "Handcrafted",
+        description: "Up to 200,000 LEI",
+        store: StoreDto(
+          name: "MAIB",
+          imageUrl:
+              "https://fci.nl/sites/default/files/MAIB%20Moldova%20Agroindbank.jpg",
+          url: "https://www.maib.md/",
+        ),
+        isApplied: false,
+        expirationDate: DateTime.now(),
+        fromCard: card,
+      ),
+      PromotionDto(
+        id: Uuid().v4(),
+        name: "Next Purchase",
+        category: "Fantastic",
+        description: "\$25 Cashback",
+        store: StoreDto(
+          name: "NVIDIA",
+          imageUrl:
+              "https://img.favpng.com/18/20/13/nvidia-logo-geforce-intel-graphics-processing-unit-png-favpng-XSEbZScDrdBvrVF5T5QJkzcr2.jpg",
+          url: "https://www.nvidia.com/",
+        ),
+        isApplied: false,
+        expirationDate: DateTime.now(),
+        fromCard: card,
+      ),
+      PromotionDto(
+        id: Uuid().v4(),
+        name: "Next Purchase",
         category: "Awesome",
         description: "\$49 Off",
         store: StoreDto(
-          name: "Turbo Tax",
+          name: "Amazon",
           imageUrl:
-              "https://blog.turbotax.intuit.com/wp-content/uploads/2019/04/cropped-favicon.png",
-          url: "https://turbotax.intuit.com/",
+              "http://media.corporate-ir.net/media_files/IROL/17/176060/Oct18/Amazon%20logo.PNG",
+          url: "https://www.amazon.com/",
         ),
         isApplied: false,
         expirationDate: DateTime.now(),
@@ -267,9 +261,23 @@ class MockData {
       ),
       PromotionDto(
         id: Uuid().v4(),
-        name: "Sleek Fresh Shirt",
+        name: "All Online Purchases",
         category: "Unbranded",
-        description: "\$1 Cashback",
+        description: "23% Off",
+        store: StoreDto(
+          name: "SPORTLANDIA",
+          imageUrl: "https://sportlandia.md/assets/img/logo.png",
+          url: "https://sportlandia.md/",
+        ),
+        isApplied: false,
+        expirationDate: DateTime.now(),
+        fromCard: card,
+      ),
+      PromotionDto(
+        id: Uuid().v4(),
+        name: "All Food",
+        category: "Unbranded",
+        description: "\$19 Off",
         store: StoreDto(
           name: "Starbucks",
           imageUrl:
@@ -282,29 +290,13 @@ class MockData {
       ),
       PromotionDto(
         id: Uuid().v4(),
-        name: "Gorgeous Fresh Cheese",
-        category: "Unbranded",
-        description: "65% Off",
-        store: StoreDto(
-          name: "Starbucks",
-          imageUrl:
-              "https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png",
-          url: "https://www.starbucks.com/",
-        ),
-        isApplied: false,
-        expirationDate: DateTime.now(),
-        fromCard: card,
-      ),
-      PromotionDto(
-        id: Uuid().v4(),
-        name: "Gorgeous Wooden Shirt",
+        name: "All Online Purchases",
         category: "Generic",
-        description: "37% Cashback",
+        description: "\$37 Cashback",
         store: StoreDto(
-          name: "Starbucks",
-          imageUrl:
-              "https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png",
-          url: "https://www.starbucks.com/",
+          name: "SPORTLANDIA",
+          imageUrl: "https://sportlandia.md/assets/img/logo.png",
+          url: "https://sportlandia.md/",
         ),
         isApplied: false,
         expirationDate: DateTime.now(),
